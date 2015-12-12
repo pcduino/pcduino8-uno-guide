6402a79beead087f39e5f92af324c795fa4d1779
YaoQ <yq000cn@gmail.com>
2015-12-12 17:22:26 +0800

    add new demo

---
+++ demo/4.NetworkVideoMonitoring/start_motion.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#get pcDuino IP address
+#on the remote PC or phone, open brower and enter the following address:
+# <IP address>:8081
+# Firefox/Chrome is recommended. 
+sudo service motion start
+echo "Please use remote brower to acccess <ip-address>:8081"
